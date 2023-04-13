9146c600e8e7c2a3641ee6bcda8c921144627bcb
Kalina Szymczyk <46633019+kalinaszy@users.noreply.github.com>
2023-04-13 22:31:16 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   required_version = ">= 1.0.11"
   backend "gcs" {
-    credentials = "C:/Users/kalin/Desktop/infra-optics-379215-5ed82b71ac17.json"
     bucket = "terraform-bucket-task8"
     prefix = "dev"
   }
