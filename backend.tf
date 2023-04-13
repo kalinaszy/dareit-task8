9e5b75b14cfd6a9a69eb8f8f9d17df82b27b007f
Kalina Szymczyk <46633019+kalinaszy@users.noreply.github.com>
2023-04-13 21:45:41 +0200

    fix: changing the name of the bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.11"
   backend "gcs" {
-    bucket = "terraform-bucket-task8-tf"
+    bucket = "terraform-bucket-task8"
     prefix = "dev"
   }
   required_providers {
